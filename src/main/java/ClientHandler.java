6c11f5ff2ad177b5c04bf609477b1291807a6973
Mads Knudsen <madsfknudsen@hotmail.com>
2021-03-08 09:30:12 +0100

    Added Basic

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,6 +1,63 @@
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientHandler implements Runnable{
+    private int id = -1;
+    private String name = "";
+    private Socket socket = null;
+    private PrintWriter pw = null;
+    private Scanner scanner = null;
+
+    public ClientHandler(int id, Socket socket) {
+        this.id = id;
+        this.socket = socket;
+
+        try {
+            pw = new PrintWriter(socket.getOutputStream(), true);
+            scanner = new Scanner(socket.getInputStream());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private boolean HandleCommand(String command){
+
+        return false;
+    }
+
     @Override
     public void run() {
-        
+        try {
+            pw.println("Du er forbundet, send en string for at få den uppercase, send 'stop' for at stoppe");
+            String message = "";
+            boolean keepRunning = true;
+            while (keepRunning) {
+                if (!name.equals("")) {
+                    try {
+                        message = scanner.nextLine();
+                    } catch (Exception e) {
+                        keepRunning = false;
+                        continue;
+                    }
+
+                    if (keepRunning)
+                        keepRunning = HandleCommand(message);
+                }
+                else{
+                    pw.println("Type In Account Name");
+                    name = scanner.nextLine();
+                }
+            }
+
+            pw.println("Forbindelsen lukker");
+            socket.close();
+        } catch (Exception e){ }
     }
+
+    //region Getters
+    public int getId() {
+        return id;
+    }
+    //endregion
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -7,4 +7,46 @@ public class Server {
     public static final  int DEFAULT_PORT = 2345;
     private ServerSocket serverSocket = null;
     private static final List<ClientHandler> handlers = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        int port = DEFAULT_PORT;
+        if(args.length > 0){
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (Exception e){
+                System.out.println("Invalid port number, using default port: " + DEFAULT_PORT);
+            }
+        }
+
+        new Server().StartServer(port);
+    }
+
+    public void StartServer(int port) throws Exception{
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server started, listening on: " + port);
+        while (true) {
+            System.out.println("Waiting for a Client");
+            Socket socket = serverSocket.accept();
+            System.out.println("New Client connected\n");
+            int id = -1;
+            for(int i = 0; i < 100_000; i++){
+                boolean check = true;
+                for (ClientHandler handler: handlers) {
+                    if(handler.getId() == i) {
+                        check = false;
+                        break;
+                    }
+                }
+
+                if(check) {
+                    id = i;
+                    break;
+                }
+            }
+
+            ClientHandler clientHandler = new ClientHandler(id, socket);
+            new Thread(clientHandler).start();
+            handlers.add(clientHandler);
+        }
+    }
 }
